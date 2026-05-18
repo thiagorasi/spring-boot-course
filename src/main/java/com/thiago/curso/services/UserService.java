17a6a59ee4f68344eaad8bca56b659a4ac150ef6
thiag <thiago.ramos.bz@gmail.com>
2026-05-17 22:22:18 -0300

    Exception handler delete

--- src/main/java/com/thiago/curso/services/UserService.java
+++ src/main/java/com/thiago/curso/services/UserService.java
@@ -4,6 +4,8 @@ import com.thiago.curso.entities.User;
 import com.thiago.curso.repositories.UserRepository;
 import com.thiago.curso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -32,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){ // id para indicar qual usuáiro será atualizado e obj para indicar os novos dados do usuário
